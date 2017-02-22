3624e4c1f8ac90a6ec00e41e4908fe35edf6dff1
Raul Gonzalez <ragore.09@gmail.com>
2017-02-21 21:59:43 -0600

    added mysql configuration and table scripts

---
+++ src/main/java/com/nearsoft/upiita/api/config/UpiitaApiApplication.java
@@ -0,0 +1,12 @@
+package com.nearsoft.upiita.api.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiitaApiApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(UpiitaApiApplication.class, args);
+    }
+}
---
+++ src/main/resources/db/migration/V1.0__Initial.sql
@@ -0,0 +1,49 @@
+CREATE TABLE director (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  origin_country VARCHAR(50) NOT NULL,
+  sex VARCHAR(5) NOT NULL,
+  CONSTRAINT director_pkey PRIMARY KEY (id),
+  CONSTRAINT director_name_ukey UNIQUE(name)
+);
+
+
+CREATE TABLE movie (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  origin_country VARCHAR(50) NOT NULL,
+  year BIGINT NOT NULL,
+  gender VARCHAR(50) NOT NULL,
+  id_director BIGINT NOT NULL,
+  CONSTRAINT  movie_pkey PRIMARY KEY (id),
+  CONSTRAINT movie_director_fkey FOREIGN KEY (id_director) REFERENCES director(id)
+);
+
+CREATE TABLE copy (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  isbn VARCHAR(100) NOT NULL,
+  id_movie BIGINT NOT NULL,
+  CONSTRAINT copy_pkey PRIMARY KEY (id),
+  CONSTRAINT copy_isbn_ukey UNIQUE (isbn),
+  CONSTRAINT copy_movie_fkey FOREIGN KEY (id_movie) REFERENCES movie(id)
+);
+
+CREATE TABLE client (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(50) NOT NULL,
+  address VARCHAR(200) NOT NULL,
+  CONSTRAINT client_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE loan (
+  id BIGINT NOT NULL  AUTO_INCREMENT,
+  creation_date TIMESTAMP NOT NULL,
+  return_date TIMESTAMP NOT NULL,
+  id_client BIGINT NOT NULL,
+  id_copy BIGINT NOT NULL,
+  CONSTRAINT loan_pkey PRIMARY KEY (id),
+  CONSTRAINT loan_client_fkey FOREIGN KEY (id_client) REFERENCES loan(id),
+  CONSTRAINT loan_copy_fkey FOREIGN KEY (id_copy) REFERENCES copy(id)
+);
